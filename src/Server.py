d1d94f2ab0b684206585b436d769e5e553e3aca6
yunhaoyang234 <47614375+yunhaoyang234@users.noreply.github.com>
2019-06-11 20:11:25 -0500

    fix python files

--- src/Server.py
+++ src/Server.py
@@ -1,28 +1,31 @@
-
 import socket
 TERMINATE = "end"
 def main():
     ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ss.bind(('', 1241))
+    ss.bind(('', 1248))
     ss.listen(5)
     while 1:
         print("Waiting on client...")
         s,a = ss.accept()
         print("Client Conected")
-
+        clientName = s.recv(4096).decode()
+        print("Begin chat")
+        serverStr = "Hi, let's begin chat"
+        s.send(serverStr.encode())
+        
         while 1:
-            print("hi")
             clientStr = s.recv(4096).decode()
+
             if clientStr == 0 or clientStr == TERMINATE:
                 print("CLIENT CLOSED")
                 break
-            print("hello")
-            print(clientStr)
+
+            print(clientName + ": " + clientStr)
             serverStr = raw_input("Me: ")
+            s.send(serverStr.encode())
             if serverStr == TERMINATE:
                 ss.close()
                 print("CONNECTION CLOSED BY SERVER")
                 exit(0)
-            print(serverStr)
-            ss.send(serverStr.encode())
+
 main()
